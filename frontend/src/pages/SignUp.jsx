78e853cc52f2bc79e22e3d691058a297b8feab58
passwordd009 <elijah.hawes47@gmail.com>
2024-05-10 18:52:11 -0400

    temp

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -9,11 +9,30 @@ export default function SignUpPage() {
   const navigate = useNavigate();
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
   const [errorText, setErrorText] = useState('');
+  const [first, setFirst] = useState('')
+  const [last, setLast] = useState('')
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [county, setCounty] = useState('');
   // We could also use a single state variable for the form data:
   // const [formData, setFormData] = useState({ username: '', password: '' });
   // What would be the pros and cons of that?
+
+  const counties = [
+    'Albany', 'Allegany', 'Bronx', 'Broome', 'Cattaraugus',
+    'Cayuga', 'Chautauqua', 'Chemung', 'Chenango', 'Clinton',
+    'Columbia', 'Cortland', 'Delaware', 'Dutchess', 'Erie',
+    'Essex', 'Franklin', 'Fulton', 'Genesee', 'Greene',
+    'Hamilton', 'Herkimer', 'Jefferson', 'Kings (Brooklyn)', 'Lewis',
+    'Livingston', 'Madison', 'Monroe', 'Montgomery', 'Nassau',
+    'New York (Manhattan)', 'Niagara', 'Oneida', 'Onondaga', 'Ontario',
+    'Orange', 'Orleans', 'Oswego', 'Otsego', 'Putnam',
+    'Queens', 'Rensselaer', 'Richmond (Staten Island)', 'Rockland', 'Saratoga',
+    'Schenectady', 'Schoharie', 'Schuyler', 'Seneca', 'Steuben',
+    'Suffolk', 'Sullivan', 'Tioga', 'Tompkins', 'Ulster',
+    'Warren', 'Washington', 'Wayne', 'Westchester', 'Wyoming',
+    'Yates'
+];
 
   if (currentUser) return <Navigate to="/" />;
 
@@ -22,7 +41,7 @@ export default function SignUpPage() {
     setErrorText('');
     if (!username || !password) return setErrorText('Missing username or password');
 
-    const [user, error] = await createUser({ username, password });
+    const [user, error] = await createUser({ first, last, username, password, county});
     if (error) return setErrorText(error.message);
 
     setCurrentUser(user);
@@ -36,9 +55,32 @@ export default function SignUpPage() {
   };
 
   return <>
-    <h1>Sign Up</h1>
+   <center><h1>Sign Up</h1></center> 
+      <br />
+    
     <form onSubmit={handleSubmit} onChange={handleChange} aria-labelledby="create-heading">
       <h2 id="create-heading">Create New User</h2>
+
+      <label htmlFor="first">First name</label>
+      <input
+        autoComplete="off"
+        type="text"
+        id="first"
+        name="first"
+        onChange={handleChange}
+        value={first}
+      />
+
+      <label htmlFor="last">Last name</label>
+      <input
+        autoComplete="off"
+        type="text"
+        id="last"
+        name="last"
+        onChange={handleChange}
+        value={last}
+      />  
+
       <label htmlFor="username">Username</label>
       <input
         autoComplete="off"
@@ -59,14 +101,30 @@ export default function SignUpPage() {
         value={password}
       />
 
+       <label htmlFor="county">County:</label>
+        <select
+          id="county"
+          name="county"
+          value={county}
+          onChange={handleChange}
+          required
+          >
+            <option value="">Select county...</option>
+             {counties.map(county => (
+               <option key={county} value={county}>{county}</option>
+               ))}
+          </select>  
+          <br />   
+        { !!errorText && <p style={{ color: 'red', fontSize: '14px' }}>{errorText}</p> }
       {/* In reality, we'd want a LOT more validation on signup, so add more things if you have time
         <label htmlFor="password-confirm">Password Confirm</label>
         <input autoComplete="off" type="password" id="password-confirm" name="passwordConfirm" />
       */}
-
+    <br />
+    
       <button>Sign Up Now!</button>
     </form>
-    { !!errorText && <p>{errorText}</p> }
-    <p>Already have an account with us? <Link to="/login">Log in!</Link></p>
+     <br />
+    <center><p>Already have an account with us? <Link to="/login">Log in!</Link></p></center>
   </>;
 }
