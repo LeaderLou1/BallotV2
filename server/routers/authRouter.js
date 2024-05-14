048db6f3538ad251cb72cd4a8402cedacda9f80f
xhenis <xhenism2001@gmail.com>
2024-05-14 13:49:39 -0400

    merged

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -6,6 +6,5 @@ const authRouter = express.Router();
 authRouter.get("/me", authControllers.showMe);
 authRouter.post("/login", authControllers.loginUser);
 authRouter.delete("/logout", authControllers.logoutUser);
-authRouter.get("/role", authControllers.checkUserRole);
 
 module.exports = authRouter;
