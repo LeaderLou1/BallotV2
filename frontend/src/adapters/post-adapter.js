3b2c8608ed86b66e64ec31e08b3b1e8046ec4859
xhenis <xhenism2001@gmail.com>
2024-05-16 14:43:20 -0400

    adapters

--- frontend/src/adapters/post-adapter.js
+++ frontend/src/adapters/post-adapter.js
@@ -1,17 +1,12 @@
 import { fetchHandler, getPostOptions, getPatchOptions, deleteOptions } from "../utils";
 
-const baseUrl = '/api/users/id:/posts'
+const baseUrl = '/api/users'
 
 export const createPost = async ({ user_id, content }) => (
-    fetchHandler(baseUrl, getPostOptions({ user_id, content }))
+    fetchHandler(`${baseUrl}/${user_id}/posts`, getPostOptions({ content }))
 );
 
-export const getAllPosts = async () => {
-    const [posts] = await fetchHandler(baseUrl);
-    return posts || [];
-};
-
-export const getPost = async (id) => fetchHandler(`${baseUrl}/${id}`);
+export const getPostByUserId = async (id) => fetchHandler(`${baseUrl}/${user_id}/posts`);
 
 export const updatePostContent = async ({ id, content }) => (
     fetchHandler(`${baseUrl}/${id}`, getPatchOptions({ id, content }))
