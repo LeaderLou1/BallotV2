aa919662bc4bfd29c2b982ffad27df524d2cd36d
xhenis <xhenism2001@gmail.com>
2024-05-21 14:10:34 -0400

    logout fix

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -4,19 +4,22 @@ import CurrentUserContext from "../contexts/current-user-context";
 
 export default function SiteHeadingAndNav() {
   const { currentUser } = useContext(CurrentUserContext);
- 
+
   return <header>
     <a id='logo' href='/'>React/Express Auth</a>
     <nav>
       <ul>
         <li><NavLink to='/'>Home</NavLink></li>
 
-        {
-            <>
-              <li><NavLink to='/users' end={true}>Users</NavLink></li>
-              <li><NavLink to='/login'>Login</NavLink></li>
-              <li><NavLink to='/choose'>Sign Up</NavLink></li>
-            </>
+        {currentUser ? <>
+          <li><NavLink to='/users' end={true}>Users</NavLink></li>
+          <li><NavLink to={`/users/${currentUser.id}`}> {currentUser.username} </NavLink></li>
+        </> :
+          <>
+            <li><NavLink to='/users' end={true}>Users</NavLink></li>
+            <li><NavLink to='/login'>Login</NavLink></li>
+            <li><NavLink to='/choose'>Sign Up</NavLink></li>
+          </>
         }
       </ul>
     </nav>
